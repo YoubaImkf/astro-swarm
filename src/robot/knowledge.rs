f2abb567d2d777c6806409362c980697c8977e76
YoubaImkf <kyox.development@gmail.com>
2025-04-19 16:01:37 +0200

    feat(knowledge): improve robot knowlede and mapping system

--- src/robot/knowledge.rs
+++ src/robot/knowledge.rs
@@ -1,7 +1,10 @@
+use log::error;
 use std::collections::HashMap;
-use crate::communication::channels::ResourceType;
 
-#[derive(Clone, Debug)]
+use crate::communication::channels::ResourceType;
+use crate::map::noise::Map;
+
+#[derive(Clone, Debug, PartialEq)]
 pub enum TileInfo {
     Unknown,
     Walkable,
@@ -19,33 +22,59 @@ pub struct RobotKnowledge {
 
 impl RobotKnowledge {
     pub fn new(width: usize, height: usize) -> Self {
-        let mut map = HashMap::new();
-        // Init all tiles are unknown
+        let capacity = width * height;
+        let mut map = HashMap::with_capacity(capacity);
+
         for y in 0..height {
             for x in 0..width {
                 map.insert((x, y), TileInfo::Unknown);
             }
         }
+
+        let center_x = width / 2;
+        let center_y = height / 2;
+        map.insert((center_x, center_y), TileInfo::Station);
         Self { map, width, height }
     }
 
     pub fn update_tile(&mut self, x: usize, y: usize, info: TileInfo) {
-        self.map.insert((x, y), info);
+        if x < self.width && y < self.height {
+            self.map.insert((x, y), info);
+        } else {
+            error!(
+                "Attempted to update knowledge out of bounds at ({}, {})",
+                x, y
+            );
+        }
     }
 
     pub fn get_tile(&self, x: usize, y: usize) -> &TileInfo {
         self.map.get(&(x, y)).unwrap_or(&TileInfo::Unknown)
     }
 
-    pub fn observe_and_update(&mut self, x: usize, y: usize, map: &crate::map::noise::Map) {
-        if map.is_station(x, y) {
-            self.update_tile(x, y, TileInfo::Station);
-        } else if map.is_obstacle(x, y) {
-            self.update_tile(x, y, TileInfo::Obstacle);
-        } else if let Some((res_type, amount)) = map.get_resource(x, y) {
-            self.update_tile(x, y, TileInfo::Resource(res_type, amount));
-        } else {
-            self.update_tile(x, y, TileInfo::Walkable);
+    pub fn observe_and_update(&mut self, x: usize, y: usize, map: &Map) {
+        if x >= map.width || y >= map.height {
+            error!("Attempted to observe map out of bounds at ({}, {})", x, y);
+            return;
         }
+
+        let info = if map.is_station(x, y) {
+            TileInfo::Station
+        } else if map.is_obstacle(x, y) {
+            TileInfo::Obstacle
+        } else if let Some((res_type, amount)) = map.get_resource(x, y) {
+            if amount > 0 {
+                TileInfo::Resource(res_type, amount)
+            } else {
+                TileInfo::Walkable
+            }
+        } else {
+            TileInfo::Walkable
+        };
+        self.update_tile(x, y, info);
+    }
+
+    pub fn get_station_coords(&self) -> (usize, usize) {
+        (self.width / 2, self.height / 2)
     }
 }
