f37b739f93536c42b4eea1129cbe76a70d31ac88
YoubaImkf <kyox.development@gmail.com>
2025-04-21 23:00:07 +0200

    test(knowledge): add unit tests

--- src/robot/core/knowledge.rs
+++ src/robot/core/knowledge.rs
@@ -78,3 +78,46 @@ impl RobotKnowledge {
         (self.width / 2, self.height / 2)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_initializes_unknown_and_station() {
+        let width = 10;
+        let height = 10;
+        let knowledge = RobotKnowledge::new(width, height);
+        for y in 0..height {
+            for x in 0..width {
+                let tile = knowledge.get_tile(x, y);
+                if (x, y) == (width / 2, height / 2) {
+                    assert_eq!(tile, &TileInfo::Station);
+                } else {
+                    assert_eq!(tile, &TileInfo::Unknown);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_update_and_get_tile() {
+        let mut knowledge = RobotKnowledge::new(5, 5);
+        knowledge.update_tile(1, 2, TileInfo::Walkable);
+        assert_eq!(knowledge.get_tile(1, 2), &TileInfo::Walkable);
+    }
+
+    #[test]
+    fn test_update_tile_out_of_bounds_does_not_panic() {
+        let mut knowledge = RobotKnowledge::new(3, 3);
+        knowledge.update_tile(10, 10, TileInfo::Obstacle);
+        assert_eq!(knowledge.get_tile(10, 10), &TileInfo::Unknown);
+    }
+
+    #[test]
+    fn test_get_station_coords() {
+        let knowledge = RobotKnowledge::new(8, 6);
+        assert_eq!(knowledge.get_station_coords(), (4, 3));
+    }
+
+}
